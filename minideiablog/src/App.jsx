c18f97b882511ebe107f6b6fa8afe4685a01953f
luisfesantos <luis.santos585@etec.sp.gov.br>
2024-08-28 19:40:49 -0300

    Deploy Automático no Firebase

--- minideiablog/src/App.jsx
+++ minideiablog/src/App.jsx
@@ -1,22 +1,12 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import ResponsiveAppBar from './navbar/NavBar';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
 import './App.css'
+import NavBar from './componentes/navbar'
 
 function App() {
 
   return (
     <>
       <div>
-        <ResponsiveAppBar />
-        <Box sx={{ width: '100%', maxWidth: 500 }}>
-          <Typography variant="h4" gutterBottom>
-            Minha página em React
-          </Typography>
-          <Button variant="contained">Hello world</Button>
-        </Box>
+        <NavBar/>
       </div>
     </>
   )
