cb4b4558c0593b2d81e10fdec8a3e7bbbdcaaf7b
luisfesantos <luis.santos585@etec.sp.gov.br>
2024-09-04 20:39:53 -0300

    Aula0904 - Estrutura de Projeto React

--- minideiablog/src/App.jsx
+++ minideiablog/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
-import NavBar from './componentes/navbar'
+import NavBar from './componentes/NavBar'
+import Footer from './componentes/Footer'
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <div>
         <NavBar/>
+        <Footer/>
       </div>
     </>
   )
